249be8af77a705b1bd6f6dd63720c7767c162de9
Robert Keith <robertfkeith@me.com>
2019-01-21 14:17:12 -0800

    remove commented code

--- chips/cc26x2/src/radio/patches/patch_cpe_prop.rs
+++ chips/cc26x2/src/radio/patches/patch_cpe_prop.rs
@@ -4,7 +4,6 @@ pub const CPE_PATCH: Patches = Patches::new();
 
 #[repr(C)]
 pub struct CPERam {
-    // rfc_ram: [VolatileCell<u32>; 252],
     rfc_ram: [u32; 252],
 }
 
@@ -69,14 +68,6 @@ impl Patches {
     }
 
     fn enter_prop_cpe_patch(&self) {
-        //let regs = unsafe { &*self.patch_vec_offset };
-        // let mut i = 0;
-        /*
-        for reg in regs.rfc_ram.iter() {
-            reg.set(PATCH_IMAGE_PROP[i]);
-            i += 1;
-        }
-        */
         for i in 0..PATCH_IMAGE_PROP.len() {
             unsafe { (*self.patch_vec_offset).rfc_ram[i] = PATCH_IMAGE_PROP[i] }
         }
