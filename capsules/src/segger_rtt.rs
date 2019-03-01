416c23a94c281653ae9784e9741f102dcfea7b1f
Louis Thiery <louis@helium.com>
2019-03-01 08:14:58 -0800

    updated UART HIL, driver and CC26x2 Impl

--- capsules/src/segger_rtt.rs
+++ capsules/src/segger_rtt.rs
@@ -190,10 +190,6 @@ impl<'a, A: hil::time::Alarm> uart::Uart<'a> for SeggerRtt<'a, A> {}
 impl<'a, A: hil::time::Alarm> uart::UartData<'a> for SeggerRtt<'a, A> {}
 
 impl<'a, A: hil::time::Alarm> uart::Transmit<'a> for SeggerRtt<'a, A> {
-    fn set_transmit_client(&self, client: &'a uart::TransmitClient) {
-        self.client.set(client);
-    }
-
     fn transmit_buffer(
         &self,
         tx_data: &'static mut [u8],
@@ -261,7 +257,6 @@ impl<'a, A: hil::time::Alarm> uart::Configure for SeggerRtt<'a, A> {
 // Dummy implementation so this can act as the underlying UART for a
 // virtualized UART MUX.  -pal 1/10/19
 impl<'a, A: hil::time::Alarm> uart::Receive<'a> for SeggerRtt<'a, A> {
-    fn set_receive_client(&self, _client: &'a uart::ReceiveClient) {}
     fn receive_buffer(
         &self,
         buffer: &'static mut [u8],
